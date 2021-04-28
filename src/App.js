25bc5abb68102e2d3b8eb07ed140955c49bf1cd0
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-04-28 17:29:32 +0800

    moving on

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./styles.css";
+import { useState } from "react";
 import GlobalStyles from "./GlobalStyles";
 
 import { NavBar } from "./AppStyles";
 import Cart from "./Components/Cart/Cart";
 export default function App() {
+  const [isToggle, setToggle] = useState(false);
   return (
     <>
       <GlobalStyles />
       <NavBar>
-        <Cart />
+        <Cart isToggle={isToggle} setToggle={setToggle} />
       </NavBar>
     </>
   );
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-import { Wrapper, CartCount, CartSideBar } from "./Styles";
-export default function () {
+import { Wrapper, CartCount, EmptyCart, CartSideBar } from "./Styles";
+export default function Cart({ setToggle, isToggle }) {
   return (
     <>
-      <Wrapper>
+      <Wrapper onClick={() => setToggle(true)}>
         <ShoppingCartIcon fontSize="large" style={{ fill: "white" }} />
         <CartCount>0</CartCount>
       </Wrapper>
+
+      <CartSideBar className={isToggle ? "expand" : "shrink"}>
+        <EmptyCart>Empty Cart</EmptyCart>
+      </CartSideBar>
     </>
   );
 }
